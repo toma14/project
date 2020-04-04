7c3f7ae6e76c22da52228b0ec127680a9e727b08
dachi tomadze <dachi.tomadze@gmail.com>
2020-04-04 17:31:04 +0400

    add css

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<title>page title</title>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css" />
+<link rel="stylesheet" href="./css/saklasocss.css" />
+
+<body>
+    <nav>
+          <div class="mydiv"> 
+          <ul class="ul2">
+         <li class="li2"><a class="a2" href="#About">About</a></li>
+         <li class="li2"><a  class="a2" href="#Cooperation">Cooperation</a></li>
+         <li class="li2"><a  class="a2" href="#Contact">Contact</a></li>
+        </ul>    
+        <ul class="ul1">
+            <li class="li1"><a  class="a1" href="#Nature">Nature</a></li>
+            <li class="li1"><a  class="a1" href="#People">People</a></li>
+            <li class="li1"><a  class="a1" href="#Trips">Trips</a></li>
+            <li class="li1"><a  class="a1" href="#Animals">Animals</a></li>
+            <li class="li1"><a  class="a1" href="#Fashion">Fashion</a></li>
+            <li class="li1"><a  class="a1" href="#Tech">Tech</a></li>
+        </ul>
+       
+        </div>
+    </nav>
+</body>
+
+</html>
