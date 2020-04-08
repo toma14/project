45a995664e7ed2f2f03cf3590bc707eee8fc43fe
dachi tomadze <dachi.tomadze@gmail.com>
2020-04-08 23:34:21 +0400

    commit message

--- index.php
+++ index.php
@@ -6,23 +6,122 @@
 
 <body>
     <nav>
-          <div class="mydiv"> 
-          <ul class="ul2">
-         <li class="li2"><a class="a2" href="#About">About</a></li>
-         <li class="li2"><a  class="a2" href="#Cooperation">Cooperation</a></li>
-         <li class="li2"><a  class="a2" href="#Contact">Contact</a></li>
-        </ul>    
-        <ul class="ul1">
-            <li class="li1"><a  class="a1" href="#Nature">Nature</a></li>
-            <li class="li1"><a  class="a1" href="#People">People</a></li>
-            <li class="li1"><a  class="a1" href="#Trips">Trips</a></li>
-            <li class="li1"><a  class="a1" href="#Animals">Animals</a></li>
-            <li class="li1"><a  class="a1" href="#Fashion">Fashion</a></li>
-            <li class="li1"><a  class="a1" href="#Tech">Tech</a></li>
-        </ul>
-       
+        <div class="div1">
+            <ul class="ul2">
+                <li class="li3"><a class="a2" href="#About">About</a></li>
+                <li class="li2"><a class="a2" href="#Cooperation">Cooperation</a></li>
+                <li class="li2"><a class="a2" href="#Contact">Contact</a></li>
+            </ul>
+            <ul class="ul1">
+                <li class="li1"><a class="a1" href="#Nature">Nature</a></li>
+                <li class="li1"><a class="a1" href="#People">People</a></li>
+                <li class="li1"><a class="a1" href="#Trips">Trips</a></li>
+                <li class="li1"><a class="a1" href="#Animals">Animals</a></li>
+                <li class="li1"><a class="a1" href="#Fashion">Fashion</a></li>
+                <li class="li1"><a class="a1" href="#Tech">Tech</a></li>
+            </ul>
+        </div>
+        <div class="div2"><i id="i" class="fas fa-search"></i></div>
+        <div class="div3">
+            <h1 class="h12">Blog</h1>
+            <h1 class="h11">Template</h1>
         </div>
     </nav>
+    <div class="div5">
+        <img src="background-bg.jpg" alt="magaria" class="img1">
+        <img src="woman.png" class="img2" alt="saswaulia">
+        <div class="div4">
+            <h1 class="h13">Maencras quis lobortis nunc. Nullam sit vel odio congue vulputate a ut nisi.</h1>
+        </div>
+    </div>
+
+    <div class="div6">
+        <div class="div7">
+            <img src="dadiani.png" class="img3">
+            <span class="span1">Donec elementum dui semper, pretium dui quis, pretium nisl.</span>
+            <span class="span2">11 september 2016 |</span>
+            <a class="span2" href="#JohnDoe">John Doe</a>
+        </div>
+        <div class="div7">
+            <img src="dadiani.png" class="img3" alt="daujerebelia">
+            <span class="span1">Maecenas quis lobortis nunc. Nullam sit amet augue vel odio congue vulputate a ut
+                nisi.</span>
+            <span class="span2">14 september 2016 |</span>
+            <a class="span2" href="#JohnDoe">John Doe</a>
+        </div>
+    </div>
+    <div class="div8">
+        <img src="gamwvaneba.png" alt="shesanishnavia" class="img4">
+        <div class="div10">
+            <h1 class="h14">Proin fongue elit fringilla</h1>
+            <span class="span3">Sodalles tillus interdum fermentum nulla. Aliquam vitae arcu condimentum, consectetur
+                diam sed, ultrices urna.</span>
+        </div>
+    </div>
+    <div class="div11">
+        <img src="gorisalucha.png" class="img5">
+        <span class="span4">Donec elementum dui semper, pretium dui quis, pretium nisl.</span>
+        <span class="span5">14 september 2016 |</span>
+        <a class="span5" href="#JohnDoe">John Doe</a>
+    </div>
+    <div class="div12">
+        <img src="fotoebi.png" class="img5">
+        <span class="span4">Donec elementum dui semper, pretium dui quis, pretium nisl.</span>
+        <span class="span5">14 september 2016 |</span>
+        <a class="span5" href="#JohnDoe">John Doe</a>
+    </div>
+    <div class="div12">
+        <img src="biltmori.png" class="img5">
+        <span class="span4">Donec elementum dui semper, pretium dui quis, pretium nisl.</span>
+        <span class="span5">14 september 2016 |</span>
+        <a class="span5" href="#JohnDoe">John Doe</a>
+    </div>
+    <div class="div11">
+        <img src="karavi.png" class="img5">
+        <span class="span4">Donec elementum dui semper, pretium dui quis, pretium nisl.</span>
+        <span class="span5">14 september 2016 |</span>
+        <a class="span5" href="#JohnDoe">John Doe</a>
+    </div>
+    <div class="div12">
+        <img src="rioni.png" class="img5">
+        <span class="span4">Donec elementum dui semper, pretium dui quis, pretium nisl.</span>
+        <span class="span5">14 september 2016 |</span>
+        <a class="span5" href="#JohnDoe">John Doe</a>
+    </div>
+    <div class="div12">
+        <img src="xidi.png" class="img5">
+        <span class="span4">Donec elementum dui semper, pretium dui quis, pretium nisl.</span>
+        <span class="span5">14 september 2016 |</span>
+        <a class="span5" href="#JohnDoe">John Doe</a>
+    </div>
+    <div class="div6">
+        <div class="div7">
+            <img src="dadiani.png" class="img3">
+            <span class="span1">Donec elementum dui semper, pretium dui quis, pretium nisl.</span>
+            <span class="span2">11 september 2016 |</span>
+            <a class="span2" href="#JohnDoe">John Doe</a>
+        </div>
+        <div class="div7">
+            <img src="dadiani.png" class="img3" alt="daujerebelia">
+            <span class="span1">Maecenas quis lobortis nunc. Nullam sit amet augue vel odio congue vulputate a ut
+                nisi.</span>
+            <span class="span2">14 september 2016 |</span>
+            <a class="span2" href="#JohnDoe">John Doe</a>
+        </div>
+        <div class="div13">
+            <div class="div16">
+                <h1 class="h15">Newsletter</h1>
+                <span class="span6">Donec elementum dui semper, pretium dui quis,pretium nisl.</span>
+            </div>
+        </div>
+        <div class="div14">
+            <div class="div15">
+                <span class="span7">Cum sociis natoque penabitus et magnis dis parturient montes, nascetur ridiculus mus.</span>
+                <span>Try awesome tool for dedsgners</span>
+                <span class="span8"> symu.co </span>
+            </div>
+        </div>
+
 </body>
 
 </html>
